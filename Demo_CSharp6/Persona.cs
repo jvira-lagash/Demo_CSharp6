270a4b6e6bef0181cc7da2e63b4a88e0af1cdc9f
developer-chile <developer.chile@outlook.com>
2015-12-26 20:40:07 -0300

    Commit inicial
    Demo C# 6

---
+++ Demo_CSharp6/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_CSharp6
+{
+    class Persona
+    {
+    }
+}
