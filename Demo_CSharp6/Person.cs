bc7b47a6f65257de7ed785e8222d8f668998a0de
developer-chile <developer.chile@outlook.com>
2015-12-27 21:47:25 -0300

    Demo CSharp 6

---
+++ Demo_CSharp6/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo_CSharp6
+{
+    class Person
+   {
+        //Before C# 6
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Person() { }
+        
+
+        //After C# 6
+
+        ////public string FirstName { get; } = "Juan";
+        ////public string LastName { get; } = "Vira";
+
+        ////public string FullName { get; }
+
+
+
+        ////public Person()
+        ////{
+        ////    //FullName = _firstName + " " + _lastName;
+        ////    FullName = this.FirstName + " " + this.LastName;
+        ////}
+    }
+}
